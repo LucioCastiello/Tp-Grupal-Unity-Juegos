0d3c25a0e30adbfb8baf7dd4ebdcdd92df8b58a4
LucioCastiello <163456589+LucioCastiello@users.noreply.github.com>
2024-06-29 19:49:02 -0300

    Agregadas scripts, pero faltan prefabs que no caen.

---
+++ Assets/Scripts/ScriptObjetos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptObjetos : MonoBehaviour
+{
+    public GameObject[] objetosPrefab; // Array de todos los productos disponibles
+    public Transform[] spawners; // Array con los spawners
+    public float delay = 2.0f; // Delay entre cada instanciación del prefab
+    public float delayDeCambio = 5.0f; // Delay entre la caída del último elemento y el cambio de pantalla
+    public static int cantidad;
+
+    void Start()
+    {
+        cantidad = Random.Range(4, 20);
+        int elemento = Random.Range(0, 20);
+        StartCoroutine(SpawnObjects(cantidad, elemento)); // Activa el proceso 'SpawnObjects'
+    }
+
+    IEnumerator SpawnObjects(int cantidad, int elemento) // Función de tipo 'IEnumerator', permite iterar los objetos y utilizar 'WaitForSeconds'
+    {
+        for (int i = 0; i < cantidad; i++)
+        {
+            int spawnerIndex = Random.Range(0, spawners.Length);
+            Instantiate(objetosPrefab[elemento], spawners[spawnerIndex].position, spawners[spawnerIndex].rotation);
+            yield return new WaitForSeconds(delay);
+        }
+
+        yield return new WaitForSeconds(delayDeCambio);
+        SceneManager.LoadScene("RESPUESTA");
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ScriptUI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BOTONES : MonoBehaviour
+{
+    public GameObject txt1;
+    public GameObject txt2;
+    public InputField inputField;
+    public GameObject titulo;
+    public GameObject escribirunnumero;
+    public GameObject panelError;
+    public GameObject cantidadnro;
+
+
+    void Start()
+    {
+        escribirunnumero.SetActive(false);
+        panelError.SetActive(false);
+    }
+
+    public void Mostrar()
+    {
+        string inputText = inputField.text;
+
+        if (int.TryParse(inputText, out int result))
+        {
+            titulo.SetActive(false);
+            cantidadnro.SetActive(false);
+            escribirunnumero.SetActive(true);
+            panelError.SetActive(false);
+
+            if (Convert.ToInt32(inputText) == ScriptObjetos.cantidad)
+            {
+                txt2.SetActive(false);
+            }
+            else
+            {
+                txt1.SetActive(false);
+            }
+        }
+        else
+        {
+            titulo.SetActive(false);
+            cantidadnro.SetActive(false);
+            escribirunnumero.SetActive(false);
+            panelError.SetActive(true);
+        }
+    }
+
+    public void Ok()
+    {
+        titulo.SetActive(true);
+        cantidadnro.SetActive(true);
+        escribirunnumero.SetActive(false);
+        panelError.SetActive(false);
+    }
+
+    public void ReRun()
+    {
+        SceneManager.LoadScene("CaenObjetos");
+    }
+}
